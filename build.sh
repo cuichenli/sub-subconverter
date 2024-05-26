26e958f9e67ebe60ccfe6cbe5a9994c4c65d8b35
Cuichen Li <cuichen.li94@gmail.com>
2024-05-26 11:51:33 +0800

    script to build the binary locally

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -xe
+
+git submodule update --init --recursive
+
+
+# setup libcron
+pushd libcron
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../deps .
+make libcron install -j3
+popd
+
+# setup quickjspp
+pushd quickjspp
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../deps .
+make install -j3
+popd
+
+# setup cmake dependencies
+rm -rf build
+conan install . --output-folder=build --build=missing
+pushd build 
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build . || true
+
+LINK_COMMAND=$(cat ./CMakeFiles/subconverter.dir/link.txt)
+eval "${LINK_COMMAND} -l:quickjs/libquickjs.a" 
